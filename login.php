223060fe57bc8bad6b33c087ef7b82d3a5e3bfb9
Nadishan Chathuranga <nadishanchathuranga21@gmail.com>
2026-02-15 20:54:30 +0530

    otp and login page done

---
+++ login.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Login -Zeylanica - Learning Portal</title>
+<link rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+      integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer" />
+
+      <!-- Owl Carousel CSS -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+
+    <link
+        href="https://fonts.googleapis.com/css2?family=Arimo:ital,wght@0,400..700;1,400..700&family=Manrope:wght@200..800&display=swap"
+        rel="stylesheet">
+ <!-- Bootstrap 5 CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="assets/css/hover.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/style-new.css">
+    <link rel="stylesheet" href="assets/css/inner-style-new.css">
+
+    <link rel="icon" href="assets/images/favicon.ico" type="image/x-icon">
+
+</head>
+
+<body>
+<div class="container-fluid login-section">
+   <div class="row align-items-stretch min-vh-100">
+      <!-- Left Side -->
+      <div class="col-lg-6 login-left d-flex flex-column">
+         <!-- Logo -->
+         <div class="col-lg-3 col-sm-5 col-6 mx-auto text-center pt-4">
+            <img src="assets/images/logo-2.png" class="w-100 mb-2" alt="Zeylanica Education">
+         </div>
+         <!-- Text Content -->
+         <div class="row flex-grow-0">
+            <div class="col-10 text-center mx-auto">
+               <h1 class="text-white optima-font font-40 text-center">
+                  Learn Anywhere, Anytime<br>Empower Your Future
+               </h1>
+               <p class="text-white manrope-font font-16 lh-base-1 pt-2 pb-3">
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                  Lorem Ipsum has been the industry's standard dummy.
+               </p>
+            </div>
+         </div>
+         <!-- Image at Bottom -->
+         <div class="row mt-auto">
+            <div class="col-10 mx-auto">
+               <img src="assets/images/l-1.png" class="w-100" alt="Zeylanica Education">
+            </div>
+         </div>
+      </div>
+      <!-- Right Side -->
+      <div class="col-lg-6 login-right d-flex align-items-center justify-content-center">
+         <div class="login-form-wrapper"  >
+            <h2 class="text-blue optima-font font-60 text-center " >Welcome</h2>
+            <p class="text-center text-blue manrope-font font-20 fw-bold pb-4">Login with Email</p>
+            <form>
+               <div class="row">
+                  <div class="col-lg-7 col-10 mx-auto mb-3">
+                     <!-- Username Field -->
+                     <div class="mb-3">
+                        <p for="username" class="login-label text-blue fw-bold font-10 manrope-font ">Username</p>
+                        <div class="input-group">
+                           <div class="input-group-text border-primary bg-transparent">
+                              <i class="fa-solid fa-envelope text-blue font-16"></i>
+                           </div>
+                           <input type="email" class="form-control text-black manrope-font font-14 border-primary py-3 bg-transparent" id="username" placeholder="thava@gmail.com" >
+                        </div>
+                     </div>
+                  </div>
+
+
+  <div class="col-lg-7 col-10 mx-auto">
+                     <!-- Username Field -->
+                     <div class="mb-3">
+                        <p for="username" class="login-label text-blue fw-bold font-10 manrope-font ">Password</p>
+                        <div class="input-group">
+                           <div class="input-group-text border-primary bg-transparent">
+                              <i class="fa-solid fa-lock text-blue font-16"></i>
+                           </div>
+                           <input type="password" class="form-control text-black manrope-font font-14 border-primary py-3 bg-transparent" id="username" placeholder="####" >
+                        </div>
+                     </div>
+                  </div>
+
+ <div class="col-lg-7 col-10 mx-auto">
+                     <!-- Username Field -->
+                     <div class="mb-3">
+             <p class="text-end">     
+                       <a class="text-secondary font-10 fw-bold " href="">Forgot your password?</a></p>
+                     </div>
+                  </div>
+               
+
+                 <div class="row justify-content-center">
+
+                      <div class="col-lg-5 col-7">
+                    <button type="submit" class="btn hvr-wobble-skew bg-red font-14 arimo-font fw-bold py-2 text-white text-center rounded-pill w-100">
+               LOGIN
+               </button>
+                </div>
+                  </div>
+                  
+               </div>
+               <!-- Login Button -->
+               
+            </form>
+         </div>
+      </div>
+   </div>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+<!-- Owl Carousel JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+ </body>
+</html>
---
+++ otp.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>OTP -Zeylanica - Learning Portal</title>
+<link rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+      integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer" />
+
+      <!-- Owl Carousel CSS -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+
+    <link
+        href="https://fonts.googleapis.com/css2?family=Arimo:ital,wght@0,400..700;1,400..700&family=Manrope:wght@200..800&display=swap"
+        rel="stylesheet">
+ <!-- Bootstrap 5 CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="assets/css/hover.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/style-new.css">
+    <link rel="stylesheet" href="assets/css/inner-style-new.css">
+
+    <link rel="icon" href="assets/images/favicon.ico" type="image/x-icon">
+
+</head>
+
+<body>
+<div class="container-fluid login-section">
+   <div class="row align-items-stretch min-vh-100">
+      <!-- Left Side -->
+      <div class="col-lg-6 login-left d-flex flex-column">
+         <!-- Logo -->
+         <div class="col-lg-3 col-sm-5 col-6 mx-auto text-center pt-4">
+            <img src="assets/images/logo-2.png" class="w-100 mb-2" alt="Zeylanica Education">
+         </div>
+         <!-- Text Content -->
+         <div class="row flex-grow-0">
+            <div class="col-10 text-center mx-auto">
+               <h1 class="text-white optima-font font-40 text-center">
+                  Learn Anywhere, Anytime<br>Empower Your Future
+               </h1>
+               <p class="text-white manrope-font font-16 lh-base-1 pt-2 pb-3">
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                  Lorem Ipsum has been the industry's standard dummy.
+               </p>
+            </div>
+         </div>
+         <!-- Image at Bottom -->
+         <div class="row mt-auto">
+            <div class="col-10 mx-auto">
+               <img src="assets/images/l-1.png" class="w-100" alt="Zeylanica Education">
+            </div>
+         </div>
+      </div>
+      <!-- Right Side -->
+      <div class="col-lg-6 login-right d-flex align-items-center justify-content-center">
+         <div class="login-form-wrapper"  >
+            <h2 class="text-blue optima-font font-60 text-center " >Enter Code</h2>
+            <p class="text-center text-blue manrope-font font-20 fw-bolder pb-4"> We sent a code to <span class="fw-bold">amelie@untiteledui.com</span></p>
+            
+<div class="row pb-4">
+    <div class="col-8 mx-auto">
+        <div class="d-flex justify-content-center">
+          <input type="text" maxlength="1" class="otp-input form-control text-center  me-3 manrope-font font-40" id="otp1" required>
+          <input type="text" maxlength="1" class="otp-input form-control text-center  me-3 manrope-font font-40" id="otp2" required>
+          <input type="text" maxlength="1" class="otp-input form-control text-center  me-3 manrope-font font-40" id="otp3" required>
+          <input type="text" maxlength="1" class="otp-input form-control text-center  me-3 manrope-font font-40" id="otp4" required>
+        </div>
+    </div>
+</div>
+
+
+            <p class="text-blue manrope-font font-16 lh-base-1 pt-2 pb-3 text-center">Didn't get a code? 
+                <a href="" class="fw-bold text-blue">
+                    Click to resend
+                </a>
+            </p>
+         </div>
+      </div>
+   </div>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+<!-- Owl Carousel JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+ </body>
+</html>
